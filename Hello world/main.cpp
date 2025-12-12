ee6dc451f28d1e498b5842f598ab6e7b6dadb72d
Kalo <g378299@gmail.com>
2025-12-11 20:24:53 -0500

    Testing classes

--- Hello world/main.cpp
+++ Hello world/main.cpp
@@ -6,6 +6,22 @@
 //
 
 #include <iostream>
+#include <string>
+
+
+
+class testing {
+    public:
+        std::string name;
+        int age;
+        int weight;
+
+}
+
+
+
+
+
 
 int foobar(){
     std::cout << "this was written in vscode!!" << std::endl;
@@ -20,6 +36,9 @@ int onWindows(){
     return 0;
 }
 
+
+
+
 int main(){
     std::cout << "Hello World" << std::endl;
     int x, y;
@@ -31,5 +50,32 @@ int main(){
     std::cout << "the numbers inputted added together are: ";
     std::cout << addNumbers(x , y) << std::endl;
     onWindows();
+
+
+    testing kalo;
+    kalo.age = 26;
+    kalo.weight = 170;
+    kalo.name = "Kalo";
+
+    testing pierre;
+    pierre.age = 19;
+    pierre.weight = 100;
+    pierre.name = "Pierre";
+
+    std::cout << "Profile 1 contains info for: " << kalo.name << std::endl;
+    std::cout << "Other Details as follows: VVV" << std::endl;
+    std::cout << "Weight: " << kalo.weight << std::endl;
+    std::cout << "Age: " << kalo.age << std::endl;
+
+    std::cout << "Profile 2 contains info for: " << pierre.name << std::endl;
+    std::cout << "Other Details as follows: VVV" << std::endl;
+    std::cout << "Weight: " << pierre.weight << std::endl;
+    std::cout << "Age: " << pierre.age << std::endl;
+
+
+
+
+
+
     return 0;
 }
